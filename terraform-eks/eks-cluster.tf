248c3e329c4eea4861d1bfc3b088a9d86f013511
Harish Kabra <harishkabra@gmail.com>
2025-02-11 16:52:27 -0500

    new

--- terraform-eks/eks-cluster.tf
+++ terraform-eks/eks-cluster.tf
@@ -9,7 +9,7 @@ module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
   version = "5.0.0"
 
-  name = "eks-vpc"
+  name = "hk-eks-vpc"
   cidr = "10.0.0.0/16"
 
   azs             = ["us-east-1a", "us-east-1b"]
@@ -17,11 +17,12 @@ module "vpc" {
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24"]
 
   enable_nat_gateway   = true
+  single_nat_gateway   = true
   enable_dns_hostnames = true
   enable_dns_support   = true
 
   tags = {
-    Name = "eks-vpc"
+    Name = "hk-eks-vpc"
   }
 }
 
@@ -132,32 +133,32 @@ module "eks" {
   source  = "terraform-aws-modules/eks/aws"
   version = "19.0.0"
 
-  cluster_name    = "my-eks-cluster"
+  cluster_name    = "hk-eks-cluster"
   cluster_version = "1.28"
 
-  iam_role_arn    = aws_iam_role.eks_cluster_role.arn
-  subnet_ids      = module.vpc.private_subnets
-  vpc_id          = module.vpc.vpc_id
+  iam_role_arn = aws_iam_role.eks_cluster_role.arn
+  subnet_ids   = module.vpc.private_subnets
+  vpc_id       = module.vpc.vpc_id
 
-  enable_irsa     = true
+  enable_irsa               = true
   cluster_security_group_id = aws_security_group.eks_security_group.id
 
   # Managed Node Group Configuration
   eks_managed_node_groups = {
     eks-node-group = {
-      desired_size = 2
+      desired_size = 1
       min_size     = 1
       max_size     = 3
 
       instance_types = ["t3.medium"]
-      key_name      = "hk-aws1-admin"  # Associate EC2 Key Pair
+      key_name       = "hk-aws1-admin" # Associate EC2 Key Pair
 
-      iam_role_arn  = aws_iam_role.eks_node_role.arn
+      iam_role_arn = aws_iam_role.eks_node_role.arn
     }
   }
 
   tags = {
     Environment = "dev"
-    Name        = "my-eks-cluster"
+    Name        = "hk-eks-cluster"
   }
 }
